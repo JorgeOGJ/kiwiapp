5e63a7798f5d61050c73f0e565033d2c55ddea58
JorgeOGJ <nuevecopitadenieve@gmail.com>
2014-12-22 16:48:02 -0600

    Depurado carga de productos

--- www/js/index.js
+++ www/js/index.js
@@ -52,3 +52,145 @@ var app = {
 };
 
 app.initialize();
+
+
+var imagenes = new Array();
+imagenes[0] = "img/banners/collares_ban.png";
+imagenes[1] = "img/banners/bolsas_ban.png";
+imagenes[2] = "img/banners/pashmina_ban.png";
+var index = 0;
+
+setInterval(function () {
+    index++;
+    if (index > 2)
+    {
+        index = 0;
+    }
+    $(".banners img").fadeOut('fast').fadeIn('fast').attr('src', imagenes[index]);
+}, 12000);
+
+function enableCategories()
+{
+    $("#categorias li").on('click', function (e)
+    {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 200);
+        $.loader({
+            className: "blue-with-image-2",
+            content: ''
+        });
+        $(".banners").hide();
+
+        var id = $(this).attr('id').replace('cat_', '', $(this).attr('id'));
+
+        $.ajax({
+            'url': 'http://localhost/kwshop/index.php?/app/pcat/',
+            'dataType': 'json',
+            'type': 'POST',
+            data: {
+                categoria: id
+            }
+        }).done(function (data)
+        {
+            $.loader('close');
+            $(".pantallas").html(data.html).show();
+            $("#productos li a").on('click', function (e)
+            {
+                e.preventDefault();
+                return false;
+            });
+        });
+
+        return false;
+    });
+
+    $("#productos li").on('click', function ()
+    {
+
+        $.loader({
+            className: "blue-with-image-2",
+            content: ''
+        });
+
+        var id = $(this).find('a').attr('href').split('/').pop();
+
+        $.ajax({
+            'url': 'http://localhost/kwshop/index.php?/app/detalle/',
+            'dataType': 'json',
+            'type': 'POST',
+            data: {
+                producto: id
+            }
+        }).done(function (data)
+        {
+            $.loader('close');
+            $(".pantallas").html(data.html).show();
+            $("#productos li a").on('click', function (e)
+            {
+                e.preventDefault();
+                return false;
+            });
+        });
+
+    });
+}
+
+$("#logo").on('click', function ()
+{
+    $(".pantallas").hide();
+    $(".banners").show();
+    $("#menu li").css('opacity', '0.5');
+});
+
+$("#perfil").on('click', function (e)
+{
+    e.preventDefault();
+    return false;
+});
+
+$("#producto").on('click', function (e)
+{
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000);
+    $("#menu li").css('opacity', '0.45');
+    $(this).css('opacity', '1.0');
+    $("#search").animate({'margin-top': '-5px'}, 500, function () {
+        $("#search").focus();
+    });
+    $.loader({
+        className: "blue-with-image-2",
+        content: ''
+    });
+
+    $(".banners").hide();
+    $.ajax({
+        'url': 'http://localhost/kwshop/index.php?/app/categorias',
+        'dataType': 'json',
+        'type': 'POST',
+    }).done(function (html)
+    {
+        $(".pantallas").html(html.html).show();
+
+        enableCategories();
+
+        $.loader('close');
+    });
+    return false;
+});
+
+$("#tiendas").on('click', function (e)
+{
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000);
+    $("#menu li").css('opacity', '0.45');
+    $(this).css('opacity', '1.0');
+    $(".banners").hide();
+    return false;
+
+})
